5b3f5fba86b2d39090be89f11644f2e190d35643
unknown <qgieng@uci.edu>
2020-07-02 03:49:34 -0700

    loading countries from API link with axios and filtering countries

--- part2_exercise_data_for_countries/src/App.js
+++ part2_exercise_data_for_countries/src/App.js
@@ -1,24 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState ,useEffect} from 'react'
+import axios from 'axios';
 
+const LoadCountry= ({country})=>{
+
+
+  return(<li>
+    name: {country.name}
+  </li>);
+}
+
+const LoadCountries=({countries})=>{
+  if(countries.length > 10)
+    return(
+      <div><p>Too many matches, specify another filter</p></div>
+    )
+  else
+      return(
+        <div>
+          {countries.map(country=>{
+            return <LoadCountry 
+                    key = {country.name+country.alpha3Code} 
+                    country = {country}/>
+          })}
+        </div>
+      )
+}
 function App() {
+  const api_link= 'https://restcountries.eu/rest/v2/all'
+  const [countries, setCountries] = useState([]);
+  const [filter, setNewFilter] = useState('');
+
+  const hook = ()=>{
+    axios
+      .get(api_link)
+      .then(response=>{
+        setCountries(response.data);
+        console.log("finished loading data");
+      });
+  };
+  useEffect(hook,[]);
+
+
+  const  handleFilterChange = (event) =>{
+    setNewFilter(event.target.value);
+    //console.log("filter changed" , filter)
+  }
+
+
+  const showCountries = countries.filter(country=> country.name.toLowerCase().indexOf(filter.toLowerCase()) !== -1);
+  console.log(showCountries);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>find countries 
+        <input 
+        value ={filter}
+        onChange={handleFilterChange}/>
+      </p>
+      <div>
+        <LoadCountries countries={showCountries}/>
+        </div>
     </div>
   );
 }
