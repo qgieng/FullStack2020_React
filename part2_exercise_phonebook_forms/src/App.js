2a295072ad8773bd7cea48921de918a3009dafe2
unknown <qgieng@uci.edu>
2020-07-03 02:07:19 -0700

    Phonebook step10 addinging functionality to delete user and replacing existing number

--- part2_exercise_phonebook_forms/src/App.js
+++ part2_exercise_phonebook_forms/src/App.js
@@ -1,13 +1,6 @@
 import React, { useState ,useEffect} from 'react'
-import axios from 'axios';
-
-
-const LoadNames= ({person})=>{
-    return(
-    <>
-    <p>{person.name}  {person.number}</p>
-    </>)
-}
+import LoadNames from './components/LoadNames'
+import contactService from './services/contacts'
 
 const App = () => {
     const [persons, setPersons] = useState([
@@ -16,82 +9,98 @@ const App = () => {
     const [ newNumber, setNewNumber ] = useState('');
     const [ filter, setNewFilter ] = useState('');
 
-    const handleNameChange= (event)=>{
-        setNewName(event.target.value);
+    const handleNameChange= (event)=>{setNewName(event.target.value);
     }
 
-    const handleNumberChange= (event)=>{
-        setNewNumber(event.target.value);
+    const handleNumberChange= (event)=>{setNewNumber(event.target.value);
     }
-
-    const handleFilterChange= (event)=>{
-        setNewFilter(event.target.value);
+    const handleFilterChange= (event)=>{setNewFilter(event.target.value);
     }
-
 
     const HandleAddName= (event)=>{
         event.preventDefault();
-        var found = persons.find(person=>person.name===newName)
-        if(found){
-            alert(`${newName} is already added to addressbook`);
-        }
+        const found = persons.find(person=> person.name === newName )
+        console.log("found person" , found);
         const addedName = {
             name: newName,
             number: newNumber
         }
-        setPersons(persons.concat(addedName));
-        console.log('set persons : ', persons)
+        
+        if(found!== null && found !== undefined){
+            const confirmation = window.confirm(`${newName} is already added to phonebook \n Would you like to replace the old number with a new one?`);
+            if(confirmation){
+                contactService.update(found.id, addedName)
+                    .then(returnedContact=>{
+                        console.log('updated contacts', returnedContact);
+                        setPersons(persons.map(person=> person.id !== found.id? person: returnedContact));
+                    })
+                setNewName('');
+                setNewNumber('');
+            }
+            return;
+        }
+        contactService.create(addedName)
+            .then(returnedName=>
+                setPersons(persons.concat(returnedName)));
         setNewName('');
+        setNewNumber('');
+    };
+
+    const handleDeleteOf= (name)=>{
+        const result = window.confirm(`Delete ${name.name}?`);
+        if(result){
+            contactService.deleteContact(name.id);
+            setPersons(persons.filter(p => p.id !==name.id));}  
     };
 
 
     const hook= () =>{
-        axios
-            .get('http://localhost:3001/persons')
-            .then(response=>{
-                setPersons(response.data);
+        contactService.getAll()
+            .then(returnedContacts=>{
+                setPersons(returnedContacts);
             })
-    }
+    };
 
     useEffect(hook,[]);
     
-  const ContactsToShow = (filter === '')? persons
+    const ContactsToShow = (filter === '')? persons
         : persons.filter((person)=>{ 
             return person.name.toLowerCase().indexOf(filter.toLowerCase()) !==-1
         })
-        console.log(ContactsToShow);
-  return (
-    <div>
-      <h2>Phonebook</h2>
-      <div>
-          <p>filter shown with
-          
-              <input 
-                value={filter}
-                onChange={handleFilterChange}/>
-          
-          </p>
-      </div>
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <div>
+                <p>filter shown with
+                    <input 
+                        value={filter}
+                        onChange={handleFilterChange}/>
+                </p>
+            </div>
 
-      <h2>Add a new</h2>
-      <form onSubmit={HandleAddName}>
-        <div>
-            name: <input value={newName}
-                onChange={handleNameChange}/>
-            <br/>
-            number: <input value= {newNumber}
-                onChange={handleNumberChange}/>
+            <h2>Add a new</h2>
+            <form onSubmit={HandleAddName}>
+                <div>
+                    name: <input value={newName}
+                        onChange={handleNameChange}/>
+                    <br/>
+                    number: <input value= {newNumber}
+                        onChange={handleNumberChange}/>
+                </div>
+                <div>
+                <button type="submit" >add</button>
+                </div>
+            </form>
+            <h2>Numbers</h2>
+            {ContactsToShow.map((person)=>{
+                return <LoadNames key={person.name+person.number} 
+                            person={person} 
+                            handleDelete={()=>{
+                                handleDeleteOf(person)}}
+                            />
+            })}
         </div>
-        <div>
-          <button type="submit" >add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      {ContactsToShow.map((person)=>{
-          return <LoadNames key={person.name+person.number} person={person}/>
-      })}
-    </div>
-  )
+    )
 }
 
 export default App
