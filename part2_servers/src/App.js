55e4b3d26f2620b6de401037d47091a914b23778
unknown <qgieng@uci.edu>
2020-07-03 21:52:52 -0700

    adding css stylings and footer

--- part2_servers/src/App.js
+++ part2_servers/src/App.js
@@ -1,12 +1,17 @@
 import React, { useState, useEffect } from 'react'
 import Note from './components/Note'
 import noteService from './services/notes'
+import Notification from './components/Notification'
+import Footer from './components/Footer.js'
+
+
+
 
 const App = (props) => {
     const [notes, setNotes] = useState([]);
     const [newNote, setNewNote] = useState('a new note...');
     const [showAll, setShowAll] = useState(true);
-    
+    const [errorMessage, setErrorMessage] = useState(null)
     const hook = () => {
       console.log('effect')
       noteService.getAll()
@@ -51,9 +56,11 @@ const App = (props) => {
                       setNotes(notes.map(note => note.id !== id ? note : returnedNote))
                   })
             .catch(error => {
-                alert(
-                  `the note '${note.content}' was already deleted from server`
-                )
+              setErrorMessage(
+                          `Note '${note.content}' was already removed from server`)
+                      setTimeout(() => {
+                                  setErrorMessage(null)
+                                        }, 5000)
       setNotes(notes.filter(n => n.id !== id))
       })
     }
@@ -68,6 +75,7 @@ const App = (props) => {
     return (
       <div>
         <h1>Notes</h1>
+        <Notification message={errorMessage} />
         <div>        
             <button onClick={() => setShowAll(!showAll)}>          
                 show {showAll ? 'important' : 'all' }        
@@ -85,7 +93,8 @@ const App = (props) => {
                 value={newNote}
                 onChange={handleNoteChange} />   
             <button type="submit">save</button>      
-        </form>       
+        </form>  
+            <Footer/>
         </div>
     )
   }
---
+++ part2_servers/src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+    const footerStyle = {
+      color: 'green',
+      fontStyle: 'italic',
+      fontSize: 16
+    }
+  
+    return (
+      <div style={footerStyle}>
+        <br />
+        <em>Note app</em>
+      </div> 
+    )
+  }
+  export default Footer;
