464a3779935e243c751dbc3614e88d8cd1c14ef4
unknown <qgieng@uci.edu>
2020-07-05 23:18:03 -0700

    changes

--- part2_servers/src/App.js
+++ part2_servers/src/App.js
@@ -1,102 +1,96 @@
 import React, { useState, useEffect } from 'react'
 import Note from './components/Note'
-import noteService from './services/notes'
 import Notification from './components/Notification'
-import Footer from './components/Footer.js'
+import Footer from './components/Footer'
+import noteService from './services/notes'
 
+const App = () => {
+  const [notes, setNotes] = useState([]) 
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState(null)
 
-
-
-const App = (props) => {
-    const [notes, setNotes] = useState([]);
-    const [newNote, setNewNote] = useState('a new note...');
-    const [showAll, setShowAll] = useState(true);
-    const [errorMessage, setErrorMessage] = useState(null)
-    const hook = () => {
-      console.log('effect')
-      noteService.getAll()
-        .then(initialNotes => {
-          setNotes(initialNotes)
-        })
-    }
-    
-
-    useEffect(hook,[]); 
-    console.log('render', notes.length, 'notes')
-
-    const addNote = (event) => { 
-            /*
-            method, which prevents the default action of submitting a form. 
-            The default action would, among other things, cause the page to reload.
-            */   
-            event.preventDefault() ;   
-            const noteObject = {
-                content: newNote,
-                date: new Date().toISOString(),
-                important: Math.random() < 0.5,
-                id: notes.length + 1,
-            }
-            noteService
-                  .create(noteObject)
-                    .then(returnedNote => {
-                      setNotes(notes.concat(returnedNote));
-                      setNewNote('')
-                      })
-            ;
-            
-    }
-
-    const toggleImportanceOf = id => {
-      const note = notes.find(n => n.id === id)
-      const changedNote = { ...note, important: !note.important }
-    
-      noteService
-            .update(id, changedNote)
-            .then(returnedNote => {
-                      setNotes(notes.map(note => note.id !== id ? note : returnedNote))
-                  })
-            .catch(error => {
-              setErrorMessage(
-                          `Note '${note.content}' was already removed from server`)
-                      setTimeout(() => {
-                                  setErrorMessage(null)
-                                        }, 5000)
-      setNotes(notes.filter(n => n.id !== id))
+  useEffect(() => {
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
       })
-    }
-   
+  }, [])
 
-    const handleNoteChange = (event) => {      
-        setNewNote(event.target.value);  
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() > 0.5,
+      id: notes.length + 1,
     }
-    
-    const notesToShow = showAll? notes: notes.filter(note=>note.important===true);
-
-    return (
-      <div>
-        <h1>Notes</h1>
-        <Notification message={errorMessage} />
-        <div>        
-            <button onClick={() => setShowAll(!showAll)}>          
-                show {showAll ? 'important' : 'all' }        
-            </button>      
-        </div>
-        <ul>
-          {notesToShow.map(note => 
-            <Note key={note.id} note={note}
-            toggleImportance={() => toggleImportanceOf(note.id)}
-            />
-          )}
-        </ul>
-        <form onSubmit={addNote}>        
-            <input 
-                value={newNote}
-                onChange={handleNoteChange} />   
-            <button type="submit">save</button>      
-        </form>  
-            <Footer/>
-        </div>
-    )
+  
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
+        setNewNote('')
+      })
   }
+
+  const toggleImportanceOf = (id) => {
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important : !note.important }
+  
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
+      .catch(error => {
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)   
+      })
+  }
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value)
+  }
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important)
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <Notification message={errorMessage} />
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all' }
+        </button>
+      </div>      
+      <ul>
+        {notesToShow.map((note, i) => 
+          <Note
+            key={i}
+            note={note} 
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
+        )}
+      </ul>
+      <form onSubmit={addNote}>
+        <input
+          value={newNote}
+          onChange={handleNoteChange}
+        />
+        <button type="submit">save</button>
+      </form>
+      <Footer />
+    </div>
+  )
+}
 
 export default App 
--- part2_servers/src/services/notes.js
+++ part2_servers/src/services/notes.js
@@ -2,16 +2,9 @@ import axios from 'axios'
 const baseUrl = '/api/notes'
 
 const getAll = () => {
-    const request = axios.get(baseUrl)
-    const nonExisting = {
-      id: 10000,
-      content: 'This note is not saved to server',
-      date: '2019-05-30T17:30:31.098Z',
-      important: true,
-    }
-    return request.then(response => response.data.concat(nonExisting))
-    //return request.then(response => response.data)
-  }
+  const request = axios.get(baseUrl)
+  return request.then(response => response.data)
+}
 
 const create = newObject => {
   const request = axios.post(baseUrl, newObject)
@@ -23,8 +16,4 @@ const update = (id, newObject) => {
   return request.then(response => response.data)
 }
 
-export default{ 
-    getAll, 
-    create, 
-    update 
-  }
+export default { getAll, create, update }
