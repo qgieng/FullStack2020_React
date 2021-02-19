75c98a5df7d074e8d11d34c6adce21be68eb0ed6
unknown <qgieng@uci.edu>
2021-02-19 02:42:45 -0800

    State forms refactor

--- part5/src/App.js
+++ part5/src/App.js
@@ -11,13 +11,13 @@ import loginService from './services/login'
 const App = () => {
 
   const [notes, setNotes] = useState([]) 
-  const [newNote, setNewNote] = useState('')
+
   const [showAll, setShowAll] = useState(true)
   const [errorMessage, setErrorMessage] = useState(null)
   const [username, setUsername] = useState('') 
   const [password, setPassword] = useState('') 
   const [user, setUser] = useState(null);
-  const [loginVisible, setLoginVisible] = useState(false);
+
 
   useEffect(() => {
     noteService
@@ -37,20 +37,11 @@ const App = () => {
     }
   }, [])
 
-  const addNote = (event) => {
-    event.preventDefault()
-    const noteObject = {
-      content: newNote,
-      date: new Date().toISOString(),
-      important: Math.random() > 0.5,
-      id: notes.length + 1,
-    }
-  
+  const addNote = (noteObject) => {
     noteService
       .create(noteObject)
       .then(returnedNote => {
         setNotes(notes.concat(returnedNote))
-        setNewNote('')
       })
   }
 
@@ -73,7 +64,7 @@ const App = () => {
       })
   }
 
-  const handleLogin = (event) => {
+  const handleLogin =async (event) => {
     event.preventDefault()
     try{
       const user = await loginService.login({
@@ -98,9 +89,7 @@ const App = () => {
     }
   }
 
-  const handleNoteChange = (event) => {
-    setNewNote(event.target.value)
-  }
+
 
   const notesToShow = showAll
     ? notes
@@ -125,11 +114,7 @@ const App = () => {
 
     const noteForm = () => (
       <Togglable buttonLabel="new note">
-        <NoteForm
-          onSubmit={addNote}
-          value={newNote}
-          handleChange={handleNoteChange}
-        />
+        <NoteForm createNote = {addNote}/>
       </Togglable>
     )
 
